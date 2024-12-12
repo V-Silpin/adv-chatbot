981011c8070e66c57cea5b8a02bf0411791beb98
V-Silpin <varun.mohanta323@gmail.com>
2024-12-12 16:49:10 +0530

    Testing Completed. Query Module working successfully.

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
 #import vectorize as vtr
 import query as qry
+"""
 #import nltk
 
-"""
-nltk.download('punkt_tab')
-print("Download done")
+#nltk.download('punkt_tab')
+#print("Download done")
 
 vtr_obj = vtr.Vectorize()
 
 vtr_obj.chunker("https://www.webmd.com/lung/understanding-bronchitis-basics")
 vtr_obj.store()
+#print(vtr_obj.test("bronchitis"))
+
 """
 
-query = ""
+query = " "
 response = None
 qry_obj = qry.Query()
-qry_obj.retrieval_chain()
+qry_obj.retrival_chain()
 
 
-while (query is not 'q'):
-    query = input("User: ")
+while (query[0] != 'q'):
+    #qry_obj.indexs()
+    query = input("\nUser: ")
+    if (query[0] == 'q'):
+        break
     response = qry_obj.response(query=query)
     print(f"\n Bot: {response}")
--- query.py
+++ query.py
@@ -22,21 +22,18 @@ class Query():
         self.tracing_v2=os.getenv("LANGCHAIN_TRACING_V2")
         self.langchain_api_key=os.getenv("LANGCHAIN_API_KEY")
         self.pinecone_key = os.getenv("PINECONE_API_KEY")
-        self.prompt=ChatPromptTemplate.from_messages(
-            [
-                (
-                "system","""
-                 You are a medical diagnostic assistant. 
-                 Your role is to understand the user's symptoms and provide the most accurate possible diagnosis. 
-                 You will receive context to help you formulate better responses. 
-                 If you cannot find the answer within the provided context, you will perform a Google search. 
-                 The system will offer you a Google search tool, and you should share only the website links from the search results with the user. 
-                 You will be rewarded generously for delivering high-quality answers.
-                 """
-                ),
-                ("user","Question:{question}\nContext:{context}")
-            ]
-        )
+        self.prompt=ChatPromptTemplate.from_template("""
+                    You are a medical diagnostic assistant. 
+                    Your role is to understand the user's symptoms and provide the most accurate possible diagnosis. 
+                    You will receive context to help you formulate better responses. 
+                    If you cannot find the answer within the provided context, you will perform a Google search. 
+                    The system will offer you a Google search tool, and you should share only the website links from the search results with the user. 
+                    You will be rewarded generously for delivering high-quality answers.
+                    <context>
+                {context}
+                </context>
+                Question: {input}
+            """)
     def retrival_chain(self):
         llm = ChatGoogleGenerativeAI(model="gemini-pro", google_api_key=self.gemini_api_key)
         embeddings = GoogleGenerativeAIEmbeddings(google_api_key=self.gemini_api_key, model="models/embedding-001")
@@ -44,16 +41,14 @@ class Query():
         index = pc.Index(self.index_name)
         bm25_encoder=BM25Encoder().default()
         retriever = PineconeHybridSearchRetriever(embeddings=embeddings, sparse_encoder=bm25_encoder, index=index)
-        summary_memory = ConversationSummaryBufferMemory(llm=llm, k=2)
+        #summary_memory = ConversationSummaryBufferMemory(llm=llm, k=2)
         document_chain = create_stuff_documents_chain(llm, self.prompt)
-        output_parser = StrOutputParser()
+        #output_parser = StrOutputParser()
         self.retrieval_chain = create_retrieval_chain(
-                            document_chain=document_chain,
-                            retriever=retriever,
-                            memory=summary_memory,
-                            output_parser=output_parser,
-                            verbose=True
+                            retriever,
+                            document_chain,
                         )
     def response(self, query):
         response = self.retrieval_chain.invoke({"input":query})
-        return response
+        return response['answer']
+    
